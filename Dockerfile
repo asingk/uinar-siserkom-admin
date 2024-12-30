ea2dd645bfb0e3367599f41b1ebeec7575c75271
asingk <asingk@proton.me>
2024-12-30 09:24:49 +0700

    update keycloak 26.0.7

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,23 @@
+FROM node:22-alpine AS base
+ENV PNPM_HOME="/pnpm"
+ENV PATH="$PNPM_HOME:$PATH"
+RUN corepack enable
+COPY . /app
+WORKDIR /app
+
+#FROM base AS prod-deps
+#RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --prod --frozen-lockfile
+
+FROM base AS build-stage
+RUN --mount=type=cache,id=pnpm,target=/pnpm/store pnpm install --frozen-lockfile
+RUN pnpm build
+# Use the lightweight Nginx image from the previous stage for the nginx container
 FROM nginx:stable-alpine
-LABEL authors="asingk"
-
-RUN apk update && \
-	apk add --no-cache tzdata
-
-ENV TZ=Asia/Jakarta
-
+# Copy the build application from the previous stage to the Nginx container
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+# Copy the nginx configuration file
 COPY nginx.conf /etc/nginx/conf.d/default.conf
-
-WORKDIR /usr/share/nginx/html
-
-# Remove default nginx static assets
-RUN rm -rf ./*
-
-# Bundle app source
-COPY dist .
-
-# Containers run nginx with global directives and daemon off
-ENTRYPOINT ["nginx", "-g", "daemon off;"]
+# Expose the port 80
+EXPOSE 80
+# Start Nginx to serve the application
+CMD ["nginx", "-g", "daemon off;"]
--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,19 @@ import '@core/scss/template/index.scss'
 import '@layouts/styles/index.scss'
 import '@styles/styles.scss'
 
-const initOptions = {
-  url: `${import.meta.env.VITE_KEYCLOAK_URL}`,
-  realm: 'uinar',
-  clientId: 'siserkom-admin',
-}
-
-const keycloak = Keycloak(initOptions)
+const keycloak = new Keycloak({
+  url: import.meta.env.VITE_KEYCLOAK_URL,
+  realm: import.meta.env.VITE_KEYCLOAK_REALM,
+  clientId: import.meta.env.VITE_KEYCLOAK_CLIENT,
+})
 
 keycloak
   .init({ onLoad: 'login-required' })
   .then(() => {
-    if (keycloak.resourceAccess['siserkom-admin']) {
+    if (keycloak.resourceAccess['siserkom-admin']?.roles[0]) {
       localStorage.setItem(
         'userRole',
-        keycloak.resourceAccess['siserkom-admin'].roles[0],
+        keycloak.resourceAccess['siserkom-admin']?.roles[0],
       )
     }
     else {
@@ -36,8 +34,7 @@ keycloak
     // Register plugins
     registerPlugins(app)
     app.config.globalProperties.keycloak = keycloak
-    // app.config.globalProperties.adminId = keycloak.tokenParsed.preferred_username
-    app.provide('adminId', keycloak.tokenParsed.preferred_username)
+    app.provide('adminId', keycloak.tokenParsed?.preferred_username)
 
     // Mount vue app
     app.mount('#app')
